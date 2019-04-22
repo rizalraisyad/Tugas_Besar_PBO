ee7a8d2452f363c45939574e22932fb0d738f912
rizalraisyad <43975267+rizalraisyad@users.noreply.github.com>
2019-04-22 16:49:57 +0700

    Add 2 Player Nama Player

--- src/Arena.java
+++ src/Arena.java
@@ -1,2 +1,16 @@
 public class Arena {
+    private Player player1;
+    private Player player2;
+
+    public void tambahPlayer(Player r1, Player r2){
+        player1 = r1;
+        player2 = r2;
+    }
+    public void tampilanPetak(){
+        int i=0;
+        int j=0;
+
+    }
+
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,19 @@
 public class Player {
+    int uang;
+    String nama="";
+
+    public  Player(String vNama){
+        nama = vNama;
+    }
+
+    public static int randomDadu(){
+        int x = (int)(Math.random()*((6-0)+1))+0;
+        return x;
+    }
+
+    public void Jalan(Player player){
+        int hasilDadu=0;
+        hasilDadu = hasilDadu + randomDadu();
+        hasilDadu = hasilDadu + randomDadu();
+    }
 }
