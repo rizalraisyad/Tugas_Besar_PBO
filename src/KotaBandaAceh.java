bf34d75f39edb758b28fa8972f3b4f833e56f322
rizalraisyad <43975267+rizalraisyad@users.noreply.github.com>
2019-05-14 16:03:48 +0700

    Rubah Class PetakTanah menjadi PijakDibeli(Semua yang bisa dibeli)

---
+++ src/KotaBandaAceh.java
@@ -0,0 +1,12 @@
+
+public class KotaBandaAceh extends Kota{
+	
+	public KotaBandaAceh() {
+		String namaPetak = "Banda Aceh";
+		String deskripsi = "Ini Banda Aceh";
+		double hargaTanah = 6000;
+	    double hargaSewaTanah = 1500;
+	    double hargaHipotik = 6600;
+	    boolean isTerjual = false;
+	}
+}
